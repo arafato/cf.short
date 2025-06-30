308a37bd6cad941ca70a8ce3e81e8bbeafac8f91
arafato <arafato@outlook.com>
2025-06-30 21:15:00 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Router } from 'itty-router';
+
+interface Env {
+	'repo': KVNamespace;
+}
+
+const router = Router();
+
+router.post('/api/create', async (request: Request, env: Env) => {
+    let data: { 
+		fullUrl?: string, 
+		alias?: string 
+	};
+
+    try {
+        data = await request.json();
+    } catch {
+        return new Response('Invalid JSON', { status: 400 });
+    }
+
+    if (!data.alias) {//
+        return new Response('Missing "alias" in request body', { status: 400 });
+    }
+	if (!data.fullUrl) {//
+        return new Response('Missing "fullUrl" in request body', { status: 400 });
+    }
+
+    await env.repo.put(data.alias, data.fullUrl);
+
+    return new Response(null, { status: 201 });
+});
+
+router.get('/:alias', async (request, env: Env) => {
+	const alias = request.params?.alias;
+	if (!alias) {
+		return new Response('Alias not provided', { status: 400 });
+	}
+	const url = await env.repo.get(`${alias}`);
+	if (url) {
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: url
+			}
+		});
+	} else {
+		return new Response('URL not found', { status: 404 });
+	}
+});
+
+router.all('*', () => new Response('Not found', { status: 404 }));
+
+export default {
+	fetch: (request, env, ctx) => router.fetch(request, env, ctx),
+} satisfies ExportedHandler<Env>;
