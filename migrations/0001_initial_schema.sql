75f3f49ada09d2f02989589c7f3ed3f9d8c1ced7
arafato <arafato@outlook.com>
2025-07-08 16:18:05 +0200

    fixed migration

--- migrations/0001_initial_schema.sql
+++ migrations/0001_initial_schema.sql
@@ -1,6 +1,7 @@
 -- Migration number: 0001 	 2025-07-08T12:37:31.277Z
 CREATE TABLE IF NOT EXISTS "user-url-mapping" (
-  userId TEXT PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId TEXT,
   originalUrl TEXT,
   shortenedUrl TEXT
 );
